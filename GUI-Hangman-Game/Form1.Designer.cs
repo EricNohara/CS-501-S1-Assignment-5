2337dc048af95dc3cee87a37300ff03f050b0d9d
Zac Lan <lanyk@bu.edu>
2025-10-07 05:35:55 -0400

    completed hangman

--- GUI-Hangman-Game/Form1.Designer.cs
+++ GUI-Hangman-Game/Form1.Designer.cs
@@ -28,20 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.wordLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.hangManPicture = new System.Windows.Forms.PictureBox();
+            this.hintWordLabel = new System.Windows.Forms.Label();
+            this.buttonLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.gamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hangManPicture)).BeginInit();
             this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Controls.Add(this.startGameButton);
+            this.gamePanel.Controls.Add(this.wordLayoutPanel);
+            this.gamePanel.Controls.Add(this.hangManPicture);
+            this.gamePanel.Controls.Add(this.hintWordLabel);
+            this.gamePanel.Controls.Add(this.buttonLayoutPanel);
+            this.gamePanel.Controls.Add(this.label1);
+            this.gamePanel.Location = new System.Drawing.Point(12, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(1473, 858);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // wordLayoutPanel
+            // 
+            this.wordLayoutPanel.AutoSize = true;
+            this.wordLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.wordLayoutPanel.Location = new System.Drawing.Point(650, 720);
+            this.wordLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.wordLayoutPanel.Name = "wordLayoutPanel";
+            this.wordLayoutPanel.Size = new System.Drawing.Size(0, 0);
+            this.wordLayoutPanel.TabIndex = 8;
+            // 
+            // hangManPicture
+            // 
+            this.hangManPicture.Image = global::GUI_Hangman_Game.Properties.Resources.Mistake_0;
+            this.hangManPicture.Location = new System.Drawing.Point(650, 112);
+            this.hangManPicture.Name = "hangManPicture";
+            this.hangManPicture.Size = new System.Drawing.Size(743, 570);
+            this.hangManPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hangManPicture.TabIndex = 7;
+            this.hangManPicture.TabStop = false;
+            // 
+            // hintWordLabel
+            // 
+            this.hintWordLabel.AutoSize = true;
+            this.hintWordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.hintWordLabel.Location = new System.Drawing.Point(73, 685);
+            this.hintWordLabel.MaximumSize = new System.Drawing.Size(512, 150);
+            this.hintWordLabel.Name = "hintWordLabel";
+            this.hintWordLabel.Size = new System.Drawing.Size(162, 55);
+            this.hintWordLabel.TabIndex = 5;
+            this.hintWordLabel.Text = "HINT: ";
+            // 
+            // buttonLayoutPanel
+            // 
+            this.buttonLayoutPanel.Location = new System.Drawing.Point(73, 112);
+            this.buttonLayoutPanel.Name = "buttonLayoutPanel";
+            this.buttonLayoutPanel.Size = new System.Drawing.Size(531, 570);
+            this.buttonLayoutPanel.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(131, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHOOSE A LETTER";
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.Location = new System.Drawing.Point(601, 354);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(212, 121);
+            this.startGameButton.TabIndex = 9;
+            this.startGameButton.Text = "Start Game";
+            this.startGameButton.UseVisualStyleBackColor = true;
+            this.startGameButton.Click += new System.EventHandler(this.startGameButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1487, 882);
+            this.Controls.Add(this.gamePanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.gamePanel.ResumeLayout(false);
+            this.gamePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hangManPicture)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel buttonLayoutPanel;
+        private System.Windows.Forms.Label hintWordLabel;
+        private System.Windows.Forms.PictureBox hangManPicture;
+        private System.Windows.Forms.FlowLayoutPanel wordLayoutPanel;
+        private System.Windows.Forms.Button startGameButton;
     }
 }
 
--- GUI-Hangman-Game/Form1.cs
+++ GUI-Hangman-Game/Form1.cs
@@ -12,9 +12,205 @@ namespace GUI_Hangman_Game
 {
     public partial class Form1 : Form
     {
+        private List<Tuple<string, string>> wordList;
+        private Button[] letterButtons;
+        private int mistakeCount;
+        private string currentWord;
+        private string currentHint;
+        private Label[] wordLabels;
+        private bool startPage = true;
+
         public Form1()
         {
             InitializeComponent();
+            PopulateWordList();
+            PopulateButtons();
+            SetPageState();
+        }
+
+        private void PopulateWordList()
+        {
+            wordList = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("APPLE", "A fruit"),
+                new Tuple<string, string>("BANANA", "A yellow fruit"),
+                new Tuple<string, string>("COMPUTER", "An electronic device"),
+                new Tuple<string, string>("HANGMAN", "A classic word guessing game"),
+                new Tuple<string, string>("GUITAR", "A musical instrument"),
+                new Tuple<string, string>("ELEPHANT", "The largest land animal"),
+                new Tuple<string, string>("JUNGLE", "A dense forest"),
+                new Tuple<string, string>("PYRAMID", "An ancient structure in Egypt"),
+                new Tuple<string, string>("OCEAN", "A large body of salt water")
+            };
+        }
+
+        private void PopulateButtons() // creating each letter button from A to Z
+        {
+            letterButtons = new Button[26];
+            char letter = 'A';
+
+            for (int i = 0; i< 26; i++)
+            {
+                letterButtons[i] = new Button
+                {
+                    Text = letter.ToString()
+                };
+
+                letterButtons[i].Click += LetterButton_Click;
+
+                buttonLayoutPanel.Controls.Add(letterButtons[i]);
+                letter++;
+                
+            }
+
+        }
+
+        private void UpdateHangManPicture()
+        {
+            string imageName = $"Mistake_{mistakeCount}";
+            hangManPicture.Image = (Image)Properties.Resources.ResourceManager.GetObject(imageName);
+
+        }
+
+        private void LetterButton_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string guessedLetter = button.Text;
+            button.Enabled = false;
+
+            if (currentWord.Contains(guessedLetter))
+            {
+                // Correct guess
+                UpdateWordDisplay(guessedLetter);
+                CheckWinCondition();
+            }
+            else
+            {
+                // Incorrect guess
+                mistakeCount++;
+                UpdateHangManPicture();
+                CheckLoseCondition();
+            }
+
+
+        }
+
+        private void UpdateWordDisplay(string guessedLetter) 
+        {
+            for (int i = 0; i < currentWord.Length; i++)
+            {
+                if (currentWord[i].ToString().ToUpper() == guessedLetter)
+                {
+                    wordLabels[i].Text = guessedLetter;
+                }
+            }
+        }
+
+        private void CheckWinCondition()
+        {
+            bool allRevealed = true;
+            foreach (var label in wordLabels)
+            {
+                if (label.Text == "__")
+                {
+                    allRevealed = false;
+                    break;
+                }
+            }
+            if (allRevealed)
+            {
+                MessageBox.Show("Congratulations, You win!");
+                startPage = true;
+                SetPageState();
+
+            }
+        }
+
+        private void CheckLoseCondition()
+        {
+            if (mistakeCount >= 6) // hangman is full
+            {
+                MessageBox.Show($"Sorry, you lost.");
+                startPage = true;
+                SetPageState();
+            }
+        }
+
+        private void PopulateWord(string currentWord) // creating labels for each letter in the word
+        {
+            wordLabels = new Label[currentWord.Length];
+            for (int i = 0; i < currentWord.Length; i++)
+            {
+                wordLabels[i] = new Label
+                {
+                    Text = "__",
+                    AutoSize = false, 
+                    Size = new Size(40, 40),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                };
+
+                wordLayoutPanel.Controls.Add(wordLabels[i]);
+            }
+        }
+
+        private void StartNewGame()
+        {
+            if (wordLabels != null) // resets labels for the given word
+            {
+                foreach (var label in wordLabels)
+                {
+                    wordLayoutPanel.Controls.Remove(label);
+                }
+            }
+
+            // resetting all variables and getting new word 
+            Random rand = new Random();
+            var selectedWord = wordList[rand.Next(wordList.Count)];
+            currentWord = selectedWord.Item1;
+            currentHint = selectedWord.Item2;
+            mistakeCount = 0;
+
+            //resetting UI
+            hintWordLabel.Text = $"Hint: {currentHint}";
+            UpdateHangManPicture();
+            foreach (var button in letterButtons)
+            {
+                button.Enabled = true;
+            }
+            PopulateWord(currentWord);
+
+
+        }
+
+        private void SetPageState() //controlling visibility of controls
+        {
+            if (startPage)
+            {
+                foreach (Control control in gamePanel.Controls)
+                {
+                    control.Enabled = false;
+                    control.Visible = false;
+                }
+                startGameButton.Enabled = true;
+                startGameButton.Visible = true;
+            } else
+            {
+                foreach (Control control in gamePanel.Controls)
+                {
+                    control.Enabled = true;
+                    control.Visible = true;
+                }
+                startGameButton.Enabled = false;
+                startGameButton.Visible = false;
+            }
+        }
+
+        private void startGameButton_Click(object sender, EventArgs e)
+        {
+            startPage = false;
+            SetPageState();
+            StartNewGame();
         }
     }
 }
